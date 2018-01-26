74192794156c15b517b626ccc1913e5203b5887a
QueenLeonidas <leoni.schulte@googlemail.com>
2018-01-26 23:21:13 +0100

    Roate Indicator

---
+++ Assets/Scripts/RotateIndicator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateIndicator : MonoBehaviour
+{
+
+    [Tooltip("Rotate speed in angle per second")]
+    public float rotateSpeed = 6f;
+    public float levitateSpeed = 8f;
+    public float levitateHeight = 16f;
+
+    // Use this for initialization
+    void Start()
+    {
+        //LeanTween.moveY(gameObject, transform.position.y + levitateHeight, levitateSpeed).setEase(LeanTweenType.easeInOutSine).setLoopPingPong();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime, Space.World);
+    }
+}
+
