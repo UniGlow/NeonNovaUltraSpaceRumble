a7c593ddc282089251347d29b9c3d10f5805e248
Mirko <mirkoskroch@gmail.com>
2018-01-28 11:42:25 +0100

    grumbleAMP deleted

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,25 +5,74 @@ using UnityEngine;
 /// <summary>
 /// 
 /// </summary>
+[RequireComponent(typeof(AudioSource))]
 public class AudioManager : SubscribedBehaviour {
-	
-	#region Variable Declarations
+
+    #region Variable Declarations
+    [SerializeField] AudioClip[] backgroundTracks;
+    [Range(0,1)]
+    [SerializeField] int[] backgroundTracksVolumes;
+    [SerializeField] AudioClip levelEnd;
+    [Range(0,1)]
+    [SerializeField] float levelEndVolume = 1f;
+
+    AudioSource src;
 	#endregion
 	
 	
 	
 	#region Unity Event Functions
 	private void Start() {
-		
+        src = GetComponent<AudioSource>();
+
+        src.PlayOneShot(GetbackgroundTrack("Track01Intro"), GetbackgroundTrackVolume("Track01Intro"));
+        StartCoroutine(StartAudioLoop());
 	}
 	
 	private void Update() {
 		
 	}
-	#endregion
-	
-	
-	
-	#region Private Functions
-	#endregion
+    #endregion
+
+
+
+    protected override void OnLevelCompleted() {
+        src.PlayOneShot(levelEnd, levelEndVolume);
+    }
+
+
+
+    #region Private Functions
+    AudioClip GetbackgroundTrack(string name) {
+        foreach (AudioClip clip in backgroundTracks) {
+            if (clip.name == name) {
+                return clip;
+            }
+        }
+        return null;
+    }
+
+    float GetbackgroundTrackVolume(string name) {
+        for (int i = 0; i < backgroundTracks.Length; i++) {
+            if (backgroundTracks[i].name == name) {
+                return backgroundTracksVolumes[i];
+            }
+        }
+        return 1f;
+    }
+    #endregion
+
+
+
+    IEnumerator StartAudioLoop() {
+        for (float i = 0; i < src.clip.length + 3f; i+=Time.deltaTime) {
+            yield return null;
+            if (!src.isPlaying) {
+                src.clip = GetbackgroundTrack("Track01Loop");
+                src.loop = true;
+                src.volume = GetbackgroundTrackVolume("Track01Loop");
+                src.Play();
+            }
+        }
+    }
 }
