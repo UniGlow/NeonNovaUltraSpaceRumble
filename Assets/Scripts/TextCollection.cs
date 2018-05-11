3c5f3dc8f94dd94c161f594c53d14fd79dadcddd
Mirko <mirkoskroch@gmail.com>
2018-05-11 03:02:30 +0200

    First implementation of Tutorial Texts

---
+++ Assets/Scripts/TextCollection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+[CreateAssetMenu(fileName = "NewTextCollection", menuName = "TextCollection")]
+public class TextCollection : ScriptableObject 
+{
+    [System.Serializable]
+	public class Message
+    {
+        public string title;
+        [TextArea(2,4)]
+        public string body;
+    }
+
+    public Message[] messages;
+}
---
+++ Assets/Scripts/UI/HeroTutorialTextUpdater.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/// <summary>
+/// 
+/// </summary>
+[RequireComponent(typeof(TextMeshPro))]
+public class HeroTutorialTextUpdater : MonoBehaviour 
+{
+
+    #region Variable Declarations
+    [SerializeField] TextCollection textCollection;
+
+    TextMeshPro textMesh;
+    Hero hero;
+	#endregion
+	
+	
+	
+	#region Unity Event Functions
+	private void Start () 
+	{
+        textMesh = GetComponent<TextMeshPro>();
+        hero = transform.parent.GetComponent<Hero>();
+
+        if (hero.ability == Ability.Damage) UpdateText("Damage");
+        else if (hero.ability == Ability.Tank) UpdateText("Tank");
+        else if (hero.ability == Ability.Opfer) UpdateText("Opfer");
+    }
+	
+	private void Update () 
+	{
+		
+	}
+	#endregion
+	
+	
+	
+	#region Public Functions
+	public void UpdateText(string title)
+    {
+        for (int i = 0; i < textCollection.messages.Length; i++)
+        {
+            if (textCollection.messages[i].title == title)
+            {
+                textMesh.text = textCollection.messages[i].body;
+            }
+        }
+    }
+	#endregion
+	
+	
+	
+	#region Private Functions
+	
+	#endregion
+}
