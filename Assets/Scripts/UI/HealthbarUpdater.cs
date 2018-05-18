959931f1023e89a21e68d26bfbf04ad331e85051
Mirko <mirkoskroch@gmail.com>
2018-05-18 21:40:20 +0200

    WobbleBobble impassible for boss

--- Assets/Scripts/UI/HealthbarUpdater.cs
+++ Assets/Scripts/UI/HealthbarUpdater.cs
@@ -12,7 +12,11 @@ public class HealthbarUpdater : MonoBehaviour
     #region Variable Declarations
     [SerializeField] RectTransform heroHealthbar;
     [SerializeField] RectTransform bossHealthbar;
-    [SerializeField] Image middleImage;
+    [SerializeField] RectTransform middleImage;
+
+    [Space]
+    [SerializeField] Sprite winBoss;
+    [SerializeField] Sprite winHeroes;
 
     float neutralWidth;
     float totalWidth;
@@ -37,18 +41,29 @@ public class HealthbarUpdater : MonoBehaviour
 	#region Public Functions
 	public void UpdateHealthbar(float heroDamage, float bossDamage)
     {
+        // Heroes winning
         if (bossDamage >= heroDamage)
         {
             heroHealthbar.sizeDelta = new Vector2(((bossDamage - heroDamage) / winningPointLeadHeroes) * neutralWidth + neutralWidth, heroHealthbar.sizeDelta.y);
             if (heroHealthbar.sizeDelta.x > totalWidth) heroHealthbar.sizeDelta = new Vector2(totalWidth, heroHealthbar.sizeDelta.y);
+
             bossHealthbar.sizeDelta = new Vector3(totalWidth - heroHealthbar.sizeDelta.x, bossHealthbar.sizeDelta.y);
+
+            middleImage.GetComponent<Image>().sprite = winHeroes;
         }
+        // Boss winning
         else
         {
             bossHealthbar.sizeDelta = new Vector2(((heroDamage - bossDamage) / winningPointLeadBoss) * neutralWidth + neutralWidth, bossHealthbar.sizeDelta.y);
             if (bossHealthbar.sizeDelta.x > totalWidth) bossHealthbar.sizeDelta = new Vector2(totalWidth, bossHealthbar.sizeDelta.y);
+
             heroHealthbar.sizeDelta = new Vector2(totalWidth - bossHealthbar.sizeDelta.x, heroHealthbar.sizeDelta.y);
+
+            middleImage.GetComponent<Image>().sprite = winBoss;
         }
+
+        Vector2 newImagePosition = new Vector2(heroHealthbar.sizeDelta.x - neutralWidth, middleImage.anchoredPosition.y);
+        middleImage.anchoredPosition = newImagePosition;
     }
 	#endregion
 	
