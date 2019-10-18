f66ed18afc7e5d8a7c9e1926b669284393e6f350
Mirko <mskroch@uniglow-entertainment.com>
2019-10-18 13:38:57 +0200

    Remap ExtensionMethod

--- Assets/Plugins/UnityGlowFramework/Utility/ExtensionMethods.cs
+++ Assets/Plugins/UnityGlowFramework/Utility/ExtensionMethods.cs
@@ -119,5 +119,19 @@ public static class ExtensionMethods
             return false;
         }
     }
+
+    /// <summary>
+    /// Remaps a value from one range to another.
+    /// </summary>
+    /// <param name="value">The value.</param>
+    /// <param name="from1">The from1.</param>
+    /// <param name="to1">The to1.</param>
+    /// <param name="from2">The from2.</param>
+    /// <param name="to2">The to2.</param>
+    /// <returns></returns>
+    public static float Remap(this float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
     #endregion
 }
