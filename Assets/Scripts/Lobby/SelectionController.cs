436362af9774debb935d144c0f3c1fe9a8879e8c
Friedemann Spitzner <email@friedemann-spitzner.de>
2019-11-11 13:39:34 +0100

    Scene Setup

---
+++ Assets/Scripts/Lobby/SelectionController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class SelectionController : MonoBehaviour 
+{
+
+    #region Variable Declarations
+    // Serialized Fields
+    [Header("References")]
+    [SerializeField] GameObject wheel = null;
+    [SerializeField] GameObject camera = null;
+    [SerializeField] List<Transform> models = new List<Transform>();
+    [SerializeField] int playerNumber = 0;
+
+    // Private
+
+    #endregion
+
+
+
+    #region Public Properties
+
+    #endregion
+
+
+
+    #region Unity Event Functions
+    private void Update()
+    {
+        
+    }
+    #endregion
+
+
+
+    #region Public Functions
+
+    #endregion
+
+
+
+    #region Private Functions
+
+    #endregion
+
+
+
+    #region GameEvent Raiser
+
+    #endregion
+
+
+
+    #region Coroutines
+
+    #endregion
+}
+
---
+++ Assets/Scripts/Lobby/SelectionUI.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+/// <summary>
+/// 
+/// </summary>
+public class SelectionUI : MonoBehaviour 
+{
+
+    #region Variable Declarations
+    // Serialized Fields
+    [Header("References")]
+    [SerializeField] GameObject topPanel = null;
+    [SerializeField] TextMeshProUGUI textMesh = null;
+    [SerializeField] RawImage renderTexture = null;
+    [SerializeField] Transform leftArrow = null;
+    [SerializeField] Transform rightArrow = null;
+    [SerializeField] Image backgroundImage = null;
+	// Private
+	
+	#endregion
+	
+	
+	
+	#region Public Properties
+	
+	#endregion
+	
+	
+	
+	#region Unity Event Functions
+	private void Start () 
+	{
+		
+	}
+	#endregion
+	
+	
+	
+	#region Public Functions
+	
+	#endregion
+	
+	
+	
+	#region Private Functions
+	
+	#endregion
+	
+	
+	
+	#region GameEvent Raiser
+	
+	#endregion
+	
+	
+	
+	#region Coroutines
+	
+	#endregion
+}
+
