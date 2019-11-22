c94d5174649f6a99f099992465bd1a70ae3e51ed
Mirko <mskroch@uniglow-entertainment.com>
2019-11-22 16:09:48 +0100

    ReadyToFight Image

--- Assets/Scripts/UI/PulsingTween.cs
+++ Assets/Scripts/UI/PulsingTween.cs
@@ -11,28 +11,59 @@ public class PulsingTween : MonoBehaviour
 {
 
     #region Variable Declarations
-    [Space]
+    [Header("Text Alpha")]
+    [Tooltip("If left empty, no alpha tweening will happen.")]
+    [SerializeField] TextMeshProUGUI textMesh = null;
     [Range(0f, 1f)]
     [SerializeField] float maxAlpha = 0.8f;
     [Range(0f, 1f)]
     [SerializeField] float minAlpha = 0.3f;
+
+    [Header("Scaling")]
+    [Tooltip("If left empty, no scaling will happen.")]
+    [SerializeField] RectTransform rectTransform = null;
     [Range(0f, 2f)]
     [SerializeField] float scaleTo = 1.2f;
     [SerializeField] float duration = 2f;
 
-    TextMeshProUGUI textMesh;
-	#endregion
-	
-	
-	
-	#region Unity Event Functions
-	private void Start () 
+    float originalAlpha;
+    Vector3 originalScale;
+    #endregion
+
+
+
+    #region Unity Event Functions
+    private void OnEnable () 
 	{
-        textMesh = GetComponent<TextMeshProUGUI>();
-        textMesh.color = new Color(textMesh.color.r, textMesh.color.g, textMesh.color.b, minAlpha);
-        DOTween.ToAlpha(() => textMesh.color, x => textMesh.color = x, maxAlpha, duration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutQuad);
-        gameObject.GetComponent<RectTransform>().DOScale(Vector3.one * scaleTo, duration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutQuad);
+        if (textMesh != null)
+        {
+            textMesh = GetComponent<TextMeshProUGUI>();
+            originalAlpha = textMesh.color.a;
+            textMesh.color = new Color(textMesh.color.r, textMesh.color.g, textMesh.color.b, minAlpha);
+            DOTween.ToAlpha(() => textMesh.color, x => textMesh.color = x, maxAlpha, duration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutQuad).SetId(this);
+        }
+
+        if (rectTransform != null)
+        {
+            originalScale = rectTransform.localScale;
+            rectTransform.DOScale(Vector3.one * scaleTo, duration).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutQuad).SetId(this);
+        }
 	}
+
+    private void OnDisable()
+    {
+        DOTween.Kill(this);
+
+        if (textMesh != null)
+        {
+            textMesh.color = new Color(textMesh.color.r, textMesh.color.g, textMesh.color.b, originalAlpha);
+        }
+
+        if (rectTransform != null)
+        {
+            rectTransform.localScale = new Vector3(originalScale.x, originalScale.y, originalScale.z);
+        }
+    }
     #endregion
 
     #region Public Functions
