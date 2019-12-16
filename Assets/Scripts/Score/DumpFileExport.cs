759595e1b7f73df4a3b8e2024bc5791e010e6eb6
Friedemann <38406798+FSpitzner@users.noreply.github.com>
2019-12-16 19:47:01 +0100

    DumpFileExport Ready to Test

--- Assets/Scripts/Score/DumpFileExport.cs
+++ Assets/Scripts/Score/DumpFileExport.cs
@@ -14,24 +14,35 @@ public static class DumpFileExport
 
 	// Private
 	static string filepath = "Assets/Resources/StatsDump.txt";
-	#endregion
-	
-	
-	
-	#region Public Properties
-	
-	#endregion
-	
-	
-	
-	#region Unity Event Functions
+    #endregion
 
-	#endregion
-	
-	
-	
-	#region Public Functions
-	public static void CreateDumpFileEntry(PlayerConfig hero1, PlayerConfig hero2, PlayerConfig hero3, PlayerConfig bossConfig, float[] matchTimes, Points points, GameSettings gameSettings)
+
+
+    #region Public Properties
+
+    #endregion
+
+
+
+    #region Unity Event Functions
+
+    #endregion
+
+
+
+    #region Public Functions
+    /// <summary>
+    /// Adds an Entry into the Dump File with all the Data needed for Balancing the Score System
+    /// </summary>
+    /// <param name="hero1">PlayerConfig of 1st Hero</param>
+    /// <param name="hero2">PlayerConfig of 2nd Hero</param>
+    /// <param name="hero3">PlayerConfig of 3rd Hero</param>
+    /// <param name="bossConfig">PlayerConfig of Boss Player</param>
+    /// <param name="matchDurations">List that contains all Match Durations in Seconds</param>
+    /// <param name="points">The Points Scriptable Object</param>
+    /// <param name="gameSettings">The GameSettings Scriptable Object</param>
+    /// <param name="version">The VersionNumber Scriptable Object</param>
+    public static void CreateDumpFileEntry(PlayerConfig hero1, PlayerConfig hero2, PlayerConfig hero3, PlayerConfig bossConfig, List<float> matchDurations, Points points, GameSettings gameSettings, VersionNumber version)
 	{
 
 		// Declaration and initialization DumpArray with all needed Information
@@ -48,11 +59,11 @@ public static class DumpFileExport
 		// Each Hero Player Stats
 		// Offset between each Player in dumpArray		!!! Change when adding any new Values to Player Specific writeouts or things will break !!!
 		int offset = 16;
-		for (int x = 0; x < 3; x++)
+		for (int a = 0; a < 3; a++)
 		{
 			// Declaring which Hero to Write
 			PlayerConfig hero;
-			switch (x)
+			switch (a)
 			{
 				case 0:
 					hero = hero1;
@@ -70,11 +81,11 @@ public static class DumpFileExport
 			List<LevelScore> heroScores = hero.HeroScore.LevelScores;
 
 			// First Line: Player Identification, AIControlled and EndScore
-			dumpArray[x * offset][0] = "Player;";
-			dumpArray[x * offset][1] = hero.ColorConfig.name + ";";
-			dumpArray[x * offset][2] = "AIControlled;";
-			dumpArray[x * offset][3] = hero.AIControlled + ";";
-			dumpArray[x * offset][4] = "EndScore;";
+			dumpArray[a * offset][0] = "Player;";
+			dumpArray[a * offset][1] = hero.ColorConfig.name + ";";
+			dumpArray[a * offset][2] = "AIControlled;";
+			dumpArray[a * offset][3] = hero.AIControlled + ";";
+			dumpArray[a * offset][4] = "EndScore;";
 			// Generating End Score
 			int endScore = 0;
 			Dictionary<ScoreCategory, int> heroEndScores = hero.HeroScore.GetScore();
@@ -82,10 +93,24 @@ public static class DumpFileExport
 			{
 				endScore += valuePair.Value;
 			}
-			dumpArray[x * offset][3] = endScore + "\n";
-			
-			// TODO: Write first Column here									!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			dumpArray[a * offset][3] = endScore + "\n";
 
+            // Fill first Column
+            dumpArray[a * offset + 1][0] = "DamageDone;";
+            dumpArray[a * offset + 2][0] = "DamageDoneScore;";
+            dumpArray[a * offset + 3][0] = "CritDamageDone;";
+            dumpArray[a * offset + 4][0] = "CritDamageDoneScore;";
+            dumpArray[a * offset + 5][0] = "ActiveTime;";
+            dumpArray[a * offset + 6][0] = "Shielded;";
+            dumpArray[a * offset + 7][0] = "BossTotalPointsDuringActivation;";
+            dumpArray[a * offset + 8][0] = "ShieldedPercentage;";
+            dumpArray[a * offset + 9][0] = "ShieldedScore;";
+            dumpArray[a * offset + 10][0] = "ActiveTime;";
+            dumpArray[a * offset + 11][0] = "OrbHeroHits;";
+            dumpArray[a * offset + 12][0] = "OrbHeroHitsScore;";
+            dumpArray[a * offset + 13][0] = "OrbBossHits;";
+            dumpArray[a * offset + 14][0] = "OrbBossHitsScore;";
+            dumpArray[a * offset + 15][0] = "ActiveTime;";
 
 			// Collecting all LevelScore Data and write it into the DumpFile
 			// Column at which to start (values only, that's why it starts at column 2)
@@ -130,25 +155,25 @@ public static class DumpFileExport
 
 			// Putting it all into the Dump Array
 				// Damage
-				dumpArray[x * offset + 1][i] = damageDone + PutRightLineEnding(i);
-				dumpArray[x * offset + 2][i] = damageDoneScore + PutRightLineEnding(i);
-				dumpArray[x * offset + 3][i] = critDamageDone + PutRightLineEnding(i);
-				dumpArray[x * offset + 4][i] = critDamageDoneScore + PutRightLineEnding(i);
-				dumpArray[x * offset + 5][i] = activeDamageTime + PutRightLineEnding(i);
+				dumpArray[a * offset + 1][i] = damageDone + PutRightLineEnding(i);
+				dumpArray[a * offset + 2][i] = damageDoneScore + PutRightLineEnding(i);
+				dumpArray[a * offset + 3][i] = critDamageDone + PutRightLineEnding(i);
+				dumpArray[a * offset + 4][i] = critDamageDoneScore + PutRightLineEnding(i);
+				dumpArray[a * offset + 5][i] = activeDamageTime + PutRightLineEnding(i);
 
 				// Tank
-				dumpArray[x * offset + 6][i] = damageShielded + PutRightLineEnding(i);
-				dumpArray[x * offset + 7][i] = bossTotalPointsDuringActivation + PutRightLineEnding(i);
-				dumpArray[x * offset + 8][i] = shieldedPercentage + PutRightLineEnding(i);
-				dumpArray[x * offset + 9][i] = shieldedScore + PutRightLineEnding(i);
-				dumpArray[x * offset + 10][i] = activeShieldTime + PutRightLineEnding(i);
+				dumpArray[a * offset + 6][i] = damageShielded + PutRightLineEnding(i);
+				dumpArray[a * offset + 7][i] = bossTotalPointsDuringActivation + PutRightLineEnding(i);
+				dumpArray[a * offset + 8][i] = shieldedPercentage + PutRightLineEnding(i);
+				dumpArray[a * offset + 9][i] = shieldedScore + PutRightLineEnding(i);
+				dumpArray[a * offset + 10][i] = activeShieldTime + PutRightLineEnding(i);
 
 				// Runner
-				dumpArray[x * offset + 11][i] = orbHeroHits + PutRightLineEnding(i);
-				dumpArray[x * offset + 12][i] = orbHeroHitsScore + PutRightLineEnding(i);
-				dumpArray[x * offset + 13][i] = orbBossHits + PutRightLineEnding(i);
-				dumpArray[x * offset + 14][i] = orbBossHitsScore + PutRightLineEnding(i);
-				dumpArray[x * offset + 15][i] = activeRunnerTime + PutRightLineEnding(i);
+				dumpArray[a * offset + 11][i] = orbHeroHits + PutRightLineEnding(i);
+				dumpArray[a * offset + 12][i] = orbHeroHitsScore + PutRightLineEnding(i);
+				dumpArray[a * offset + 13][i] = orbBossHits + PutRightLineEnding(i);
+				dumpArray[a * offset + 14][i] = orbBossHitsScore + PutRightLineEnding(i);
+				dumpArray[a * offset + 15][i] = activeRunnerTime + PutRightLineEnding(i);
 
 				i++;
 			}
@@ -162,15 +187,97 @@ public static class DumpFileExport
 		dumpArray[lineIdentifier][3] = bossConfig.AIControlled + ";";
 		
 		lineIdentifier++;
-		
+
+        // First Column
+        dumpArray[lineIdentifier][0] = "DamageDone;";
+        dumpArray[lineIdentifier + 1][0] = "CritDamageDone;";
+        dumpArray[lineIdentifier + 2][0] = "DamageShieled;";
+
 		// Values
+        for(int i = 0; i < points.BossDamageInLevels.Count; i++)
+        {
+            dumpArray[lineIdentifier][i + 1] = points.BossDamageInLevels[i] + PutRightLineEnding(i);
+            dumpArray[lineIdentifier + 1][i + 1] = points.BossCritDamageInLevels[i] + PutRightLineEnding(i);
+            dumpArray[lineIdentifier + 2][i + 1] = points.BossDamageShieldedInLevels[i] + PutRightLineEnding(i);
+        }
+        lineIdentifier += 3;
+
+        // LevelStats
+        dumpArray[lineIdentifier][0] = "Winner;";
+        for(int i = 0; i < points.WinningFactions.Count; i++)
+        {
+            dumpArray[lineIdentifier][i + 1] = points.WinningFactions[i] + PutRightLineEnding(i);
+        }
+        lineIdentifier++;
+
+        dumpArray[lineIdentifier][0] = "LevelDuration;";
+        for(int i = 0; i < matchDurations.Count; i++)
+        {
+            dumpArray[lineIdentifier][i + 1] = matchDurations[i] + PutRightLineEnding(i);
+        }
+        lineIdentifier++;
+
+        // General Information
+        dumpArray[lineIdentifier][0] = "General;";
+        lineIdentifier++;
+        dumpArray[lineIdentifier][0] = "VersionNumber;";
+        dumpArray[lineIdentifier][1] = version.versionNumber + ";";
+        lineIdentifier++;
+
+        // Game Settings
+        dumpArray[lineIdentifier][0] = "PointsLeadToWin;";
+        dumpArray[lineIdentifier][1] = gameSettings.PointLeadToWin + ";";
+        lineIdentifier++;
+
+        dumpArray[lineIdentifier][0] = "CritDamageMultiplier;";
+        dumpArray[lineIdentifier][1] = gameSettings.CritDamageMultiplier + ";";
+        lineIdentifier++;
+
+        dumpArray[lineIdentifier][0] = "IntensifyTime;";
+        dumpArray[lineIdentifier][1] = gameSettings.IntensifyTime + ";";
+        lineIdentifier++;
+
+        dumpArray[lineIdentifier][0] = "IntensifyAmount;";
+        dumpArray[lineIdentifier][1] = gameSettings.IntensifyAmount + ";";
+        lineIdentifier++;
+
+        dumpArray[lineIdentifier][0] = "BossColorSwitchInterval;";
+        dumpArray[lineIdentifier][1] = gameSettings.BossColorSwitchInterval + ";";
+        lineIdentifier++;
+
+        dumpArray[lineIdentifier][0] = "OptimalPointsPerSecond;";
+        dumpArray[lineIdentifier][1] = gameSettings.OptimalScorePerSecond + ";";
+        lineIdentifier++;
+
+        // Define writer string and fill it with all the Information contained in the DumpArray
+        string dumpText = "";
+        for(int i = 0; i < dumpArray.Length; i++)
+        {
+            for(int j = 0; j < dumpArray[i].Length; j++)
+            {
+                dumpText += dumpArray[i][j];
+            }
+        }
+        // Add End to file for readability
+        dumpText += "\n\n";
+        
+        // Write at Fileend or Create a new one if it doesn't exist
+        using (StreamWriter sw = File.AppendText(filepath))
+        {
+            sw.Write(dumpText);
+        }
 	}
-	#endregion
-	
-	
-	
-	#region Private Functions
-	private static string PutRightLineEnding(int i)
+    #endregion
+
+
+
+    #region Private Functions
+    /// <summary>
+    /// Returns ";" on default or "\n" when i == 5
+    /// </summary>
+    /// <param name="i">Column</param>
+    /// <returns></returns>
+    private static string PutRightLineEnding(int i)
 	{
 		if (i == 5)
 		{
