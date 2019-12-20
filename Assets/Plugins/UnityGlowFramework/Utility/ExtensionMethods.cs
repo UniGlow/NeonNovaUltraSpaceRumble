8761ee570392ffe4b2bbdd9aa0c63e13811148b8
Mirko <mskroch@uniglow-entertainment.com>
2019-12-20 11:59:04 +0100

    Rewrote Remap function

--- Assets/Plugins/UnityGlowFramework/Utility/ExtensionMethods.cs
+++ Assets/Plugins/UnityGlowFramework/Utility/ExtensionMethods.cs
@@ -131,7 +131,7 @@ public static class ExtensionMethods
     /// <returns></returns>
     public static float Remap(this float value, float from1, float to1, float from2, float to2)
     {
-        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+        return to1 + (value - from1) * (to2 - to1) / (from2 - from1);
     }
     #endregion
 }
