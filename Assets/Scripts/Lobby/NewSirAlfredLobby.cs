fc85fb55f069e5c70e38cf3350164364191d1295
Friedemann Spitzner <email@friedemann-spitzner.de>
2019-12-21 11:15:25 +0100

    Update NewSirAlfredLobby.cs

--- Assets/Scripts/Lobby/NewSirAlfredLobby.cs
+++ Assets/Scripts/Lobby/NewSirAlfredLobby.cs
@@ -126,7 +126,8 @@ public class NewSirAlfredLobby : MonoBehaviour
         // Reset Points
         points.ResetPoints();
         // Delete last Score Session
-        bossConfig.HeroScore.ClearAllLevels();
+        if(bossConfig.HeroScore != null)
+            bossConfig.HeroScore.ClearAllLevels();
         hero1Config.HeroScore.ClearAllLevels();
         hero2Config.HeroScore.ClearAllLevels();
         hero3Config.HeroScore.ClearAllLevels();
