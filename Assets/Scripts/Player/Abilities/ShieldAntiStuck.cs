8d5fd2c17e9745a686d9ac7853650c2834d803ac
Friedemann Spitzner <email@friedemann-spitzner.de>
2020-02-25 12:59:00 +0100

    #231 Shield spawning inside Boss bugfix
    Shield can still spawn inside the Boss, but will be temporarely disabled (the collider) until the Shield left the Boss Collider

---
+++ Assets/Scripts/Player/Abilities/ShieldAntiStuck.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+
+[RequireComponent(typeof(Collider))]
+public class ShieldAntiStuck : MonoBehaviour 
+{
+
+	#region Variable Declarations
+	// Serialized Fields
+	
+	[SerializeField] float physicsRelativeXShapeOffset = 0f;
+	[SerializeField] float physicsRelativeYShapeOffset = 0f;
+	[SerializeField] Vector3 physicsCubeDimensions = Vector3.zero;
+	// Private
+	bool blocked = false;
+	Collider shieldCollider = null;
+	#endregion
+
+
+
+	#region Public Properties
+
+	#endregion
+
+
+
+	#region Unity Event Functions
+	private void Awake()
+	{
+		if (shieldCollider == null)
+			shieldCollider = GetComponent<Collider>();
+	}
+	private void OnEnable () 
+	{
+		Debug.Log("Starting Anti-Stuck Protocol");
+		Collider[] touchingColliders = Physics.OverlapBox(transform.position + (transform.forward * physicsRelativeXShapeOffset) + (transform.up * physicsRelativeYShapeOffset), physicsCubeDimensions, Quaternion.LookRotation(transform.forward, transform.up));
+		if (touchingColliders.Length != 0)
+		{
+			foreach(Collider c in touchingColliders)
+			{
+				if(c.tag == Constants.TAG_BOSS)
+				{
+					blocked = true;
+				}
+			}
+		}
+		else
+			blocked = false;
+
+		if (blocked)
+			shieldCollider.enabled = false;
+		else
+			shieldCollider.enabled = true;
+	}
+
+	private void FixedUpdate()
+	{
+		if (blocked) {
+			Collider[] touchingColliders = Physics.OverlapBox(transform.position + (transform.forward * physicsRelativeXShapeOffset) + (transform.up * physicsRelativeYShapeOffset), physicsCubeDimensions, Quaternion.LookRotation(transform.forward, transform.up));
+			if (touchingColliders.Length != 0)
+			{
+				foreach (Collider c in touchingColliders)
+				{
+					blocked = false;
+					if (c.tag == Constants.TAG_BOSS)
+					{
+						blocked = true;
+					}
+				}
+			}
+			else
+				blocked = false;
+		}
+		else shieldCollider.enabled = true;
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.DrawWireCube(transform.position + (transform.forward * physicsRelativeXShapeOffset) + (transform.up * physicsRelativeYShapeOffset), physicsCubeDimensions);
+	}
+	#endregion
+
+
+
+	#region Public Functions
+
+	#endregion
+
+
+
+	#region Private Functions
+
+	#endregion
+
+
+
+	#region GameEvent Raiser
+
+	#endregion
+
+
+
+	#region Coroutines
+
+	#endregion
+}
+
